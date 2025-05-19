0051ee225141399c421540313d38543095680224
Emmanuelnnadi111 <doneonyi920@gmail.com>
2025-05-19 20:15:43 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to NaijaLink </h1>
+      <p>Connect with clients and artisans</p>
+    </>
+  );
+}
