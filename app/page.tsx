c914b414c89426ee711c2a6da76df45751bb07d4
Emmanuelnnadi111 <doneonyi920@gmail.com>
2025-05-19 20:54:37 +0100

    added styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="bg-white h-screen text-black text-center flex flex-col items-center justify-center">
       <h1 className="bg-yellow-200 text-white">Welcome to NaijaLink </h1>
       <p>Connect with clients and artisans</p>
+      <h1 className="bg-yellow-200 text-white">Welcome to NaijaLink </h1>
+      <p>Connect with clients and artisans</p>
     </div>
   );
 }
