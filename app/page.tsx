8e9519fc1b0ef1603efc613f4bc484b305796c1f
Emmanuelnnadi111 <doneonyi920@gmail.com>
2025-05-19 20:53:26 +0100

    added some text

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to NaijaLink </h1>
+    <div className="bg-white h-screen text-black text-center flex flex-col items-center justify-center">
+      <h1 className="bg-yellow-200 text-white">Welcome to NaijaLink </h1>
       <p>Connect with clients and artisans</p>
-    </>
+    </div>
   );
 }
